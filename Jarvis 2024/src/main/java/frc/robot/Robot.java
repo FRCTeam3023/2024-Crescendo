236138148d0abae14f3acf6307488552bf3be1e4
PixelPeely <jeremiahrugroden@gmail.com>
2024-03-27 22:39:52 -0500

    Completed Auto Aim V2

--- Jarvis 2024/src/main/java/frc/robot/Robot.java
+++ Jarvis 2024/src/main/java/frc/robot/Robot.java
@@ -6,6 +6,9 @@ package frc.robot;
 
 import java.util.Optional;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -14,6 +17,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Subsystems.Intake;
 import frc.robot.Subsystems.Pivot;
 import frc.robot.Subsystems.Pivot.PivotState;
+import frc.robot.Util.AutoAimCalculatorV2;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
